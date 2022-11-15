bb0a53fc555745d428845978628aa7c2c6bc8c21
Alan Pham <alanp7@uci.edu>
2022-11-14 20:26:56 -0800

    rest of the app, themed

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,13 @@
 export function Settings() {
+  const colors = {
+    primary: "#81968F",
+    secondary: "#96BDC6",
+    tertiary: "#FFF5F5",
+  };
   return (
-    <div style={{ margin: 20 }}>
+    <div
+      style={{ padding: 30, minHeight: 1080, backgroundColor: colors.tertiary }}
+    >
       <h1>This is the settings page.</h1>
     </div>
   );
