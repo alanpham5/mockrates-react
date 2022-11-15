cec3e5b111a55b3a5a1cfc5a9152ac4457af17dd
Alan Pham <alanp7@uci.edu>
2022-11-14 19:30:43 -0800

    barely functional chat page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.css";
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({ transparent, backgroundColor, label, ...props }) => {
-  const mode = transparent ? 'storybook-button--secondary' : 'storybook-button--primary' ;
+  const mode = transparent
+    ? "storybook-button--secondary"
+    : "storybook-button--primary";
   return (
     <button
-      type="button"
-      className={['storybook-button', `storybook-button--large`, mode].join(' ')}
+      type={props.type ?? "button"}
+      className={["storybook-button", `storybook-button--large`, mode].join(
+        " "
+      )}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
@@ -18,7 +22,6 @@ export const Button = ({ transparent, backgroundColor, label, ...props }) => {
     </button>
   );
 };
-
 
 Button.propTypes = {
   /**
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,15 @@
 import { Message } from "../components/Message";
+import { useState } from "react";
+import { Button } from "../components/Button";
 
 export function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setMessages([...messages, text]);
+    setText("");
+  };
   return (
     <div>
       <h1>This is the chat page.</h1>
@@ -9,10 +18,17 @@ export function Chat() {
         text="We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy...I just want to tell you how I'm feeling Gotta make you understand. Never gonna give you up, never gonna let you down Never gonna run around and desert you "
         isReceived={true}
       />
-      <Message
-        backgroundColor={"#81968F"}
-        text="Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you"
-      />
+      {messages.map((message) => {
+        return <Message backgroundColor={"#81968F"} text={message} />;
+      })}
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button backgroundColor={"#81968F"} label="Send" type="submit" />
+      </form>
     </div>
   );
 }
