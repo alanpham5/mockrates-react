38e7bc446f4f6b310e0f15a64e142caad777d441
Monika Phuengmak <monika.pphuengmak@gmail.com>
2022-11-18 16:48:39 -0800

    access, read, write from firebase

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -4,6 +4,44 @@ import { Button } from "../components/Button";
 import "./Chat.css";
 import { useColor } from "../api/hooks/useColor";
 
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { setDoc, getFirestore, doc, Timestamp, arrayUnion, updateDoc, } from "firebase/firestore";
+import { useDocumentOnce } from 'react-firebase-hooks/firestore';
+
+
+// Firebase configuration for SOCRATES-DEMO database
+// Need to make authentication work. Currently permission denied error.
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCMyM1CXcwj4rBoHEeq8hUPBnr0VI21Co0",
+//   authDomain: "socratesdemo-99f36.firebaseapp.com",
+//   projectId: "socratesdemo-99f36",
+//   storageBucket: "socratesdemo-99f36.appspot.com",
+//   messagingSenderId: "969208138660",
+//   appId: "1:969208138660:web:7af41e39da6d5db4f40f31",
+//   measurementId: "G-PHVFGTQQ0N"
+// }
+
+
+// Firebase configuration for PLAY-WITH-FIRESTORE database
+const firebaseConfig = {
+  apiKey: "AIzaSyC9mJSVM2UnlZamuw0zSoGosyklGUShOEI",
+  authDomain: "play-with-firestore-dc032.firebaseapp.com",
+  projectId: "play-with-firestore-dc032",
+  storageBucket: "play-with-firestore-dc032.appspot.com",
+  messagingSenderId: "333488195018",
+  appId: "1:333488195018:web:97862afe30715a0c08bb33"
+};
+
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);     // For authentication with sponsors' firestore
+const db = getFirestore(app);     // db is the instance of Firestore
+
+// Supposed the user has a user ID "4".
+const uid = 2;
+
+
+
 const sockyMessages = [
   {
     text: "Hi! It's your mental health companion, Socky! How are you feeling today?",
@@ -27,17 +65,121 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+
+// WORKS
+// Create a new decoument in message collection
+async function createUserMessage (id, text) {
+  var docRef = doc(db, "messages", id.toString());
+  await setDoc(docRef, {
+            user_id: id,
+            date: Timestamp.now(),
+            text_seq: arrayUnion(text)
+  }).then(() => {
+    alert("Data added.");
+  })
+  .catch((error) => {
+    alert("Error occured: " + error);
+  });
+}
+
+
+// WORKS
+// Append one text to text_seq array
+async function addToTextSeq(id, text) {
+  var docRef = doc(db, "messages", id.toString());
+  await updateDoc(docRef, {
+            text_seq: arrayUnion(text)
+  }).then(() => {
+    alert("Data Updated.");
+  })
+  .catch((error) => {
+    alert("Error occured: " + error);
+  });
+}
+
+
+// WORK
+// Get the user's activeDays from Firestore
+function GetActiveDays(snapshot) {
+
+    var activeDays = snapshot.get("activeDays");
+    alert(activeDays);
+
+}
+
+
+// NOT FINISHED
+// Add new user if new user. If user exist, update text.
+async function addMessagesFunction(id, text) {
+  const docRef = doc(db, "messages", id.toString());
+  await docRef.get().then((docSnapshot) => {
+    if (docSnapshot.exists) {
+      docRef.onSnapshot((doc) => {
+        addToTextSeq(doc, id, text);
+      });
+    } else {
+      createUserMessage (docRef, id, text);
+    }
+  });
+}
+
+
+// NOT FINISHED
+// Purpose: For each message that the user sends, compare it with the most recent activity in Firestore.
+// If current timestamp > most recent record, new day happens. Increment activeDays by 1.
+// Reset inactiveDaysLeft to 2.
+// TODO: Research JS syntax and map.
+async function getMostRecentActiveDate(docSnapshot) {
+
+  // mostRecent = array of maps?
+  var date_array = docSnapshot.get("allDates");
+  var sorted_date_array = date_array.keys().sort();
+  console.log(sorted_date_array);
+
+}
+
+
+
+
+
+
 export function Chat() {
+
+  console.log("In Chat()");
+
+  // messages is a list of messages between Socky and the user.
   const [messages, setMessages] = useState([]);
+
+  // text is a string containing the content of each message.
   const [text, setText] = useState("");
   const { colors } = useColor();
+
+  // const [msg_snapshot, msg_loading, msg_error, msg_reload] = useDocumentOnce(doc(db, "messages", uid.toString()));
+  const [act_snapshot, act_loading, act_error, act_reload] = useDocumentOnce(doc(db, "activityCount", uid.toString()));
+
+
+
+  // Defining onSubmit for when the user replies.
+  // When the user tap send, shallow copy the prevMessage list.
+  // Add the new {message.text, message.received} to the list.
   const onSubmit = (event) => {
     event.preventDefault();
     setMessages((prevMessages) => [
       ...prevMessages,
       { text: text, received: false },
     ]);
+
+    
+    // These works. Try it by uncommenting one at a time.
+    // createUserMessage(uid, text);
+    // addToTextSeq(uid, text);
+    // GetActiveDays(act_snapshot);
+
+    
+    // Reset text to empty string
     setText("");
+
+    // Wait for 3000 ms, if there are fewer than 6 back and forth responses, Socky replies.
     sleep(3000).then(() => {
       if (messages.length < 6) {
         setMessages((prevMessages) => [
@@ -48,7 +190,12 @@ export function Chat() {
     });
   };
 
+  // What the Chat function displays.
   return (
+
+    // Socky's chat bubble is primary color.
+    // The user's chat bubble is secondary color.
+    // Background color is tertiary color.
     <div
       style={{
         padding: 30,
@@ -62,10 +209,11 @@ export function Chat() {
         <h1>This is the chat page.</h1>
         <Message
           backgroundColor={colors.primary}
-          text={sockyMessages[0].text}
+          text={sockyMessages[0].text}      // Socky sends the first message and waits.
           isReceived={true}
         />
-        {messages.map((message) => {
+
+        {messages.map((message) => {        // Turn each text in messages list to a Message component.
           return (
             <Message
               backgroundColor={colors.primary}
@@ -82,7 +230,7 @@ export function Chat() {
         }}
       />
       <footer>
-        <form
+        <form                               // Reply box at the bottom for the user
           onSubmit={onSubmit}
           style={{
             flexShrink: 10,
@@ -111,6 +259,9 @@ export function Chat() {
           />
         </form>
       </footer>
+
     </div>
+
+
   );
 }
