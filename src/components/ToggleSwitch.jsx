edf4a13d04a5da7e8980b3c6a11548dc5152aa59
Alan <alanp7@uci.edu>
2022-11-20 21:54:32 -0800

    Add linter

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import './ToggleSwitch.css'
+import "./ToggleSwitch.css";
 
-export function ToggleSwitch({backgroundColor}){
-    return (
-        <>
-            <input
-                className="react-switch-checkbox"
-                id={`react-switch-new`}
-                type="checkbox"
-            />
-            <label
-                className="react-switch-label"
-                style={{backgroundColor: backgroundColor}}
-                htmlFor={`react-switch-new`}
-            >
-                <span className={`react-switch-button`} />
-            </label>
-        </>
-    );
+export function ToggleSwitch({ backgroundColor }) {
+  return (
+    <>
+      <input
+        className="react-switch-checkbox"
+        id={`react-switch-new`}
+        type="checkbox"
+      />
+      <label
+        className="react-switch-label"
+        style={{ backgroundColor: backgroundColor }}
+        htmlFor={`react-switch-new`}
+      >
+        <span className={`react-switch-button`} />
+      </label>
+    </>
+  );
 }
